8ee9b2eccc2eff95684e174ebfb4ea793583ce19
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-09-25 18:36:19 +0200

    Eliminada etiqueta script.

--- TEMA 2 - Integración/EJERCICIO 2/index.php
+++ TEMA 2 - Integración/EJERCICIO 2/index.php
@@ -4,7 +4,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link href="styles.css" rel="stylesheet">
     <title>EJERCICIO 2 - DANIEL ALONSO LÁZARO</title>
-    <script src="main.js"></script>
 </head>
 <body>
 <form class="main" action="info.php" method="GET" id="form">
@@ -16,6 +15,5 @@
     <input type="date" id="fecha" name="fecha" value="<?= date("Y-m-d") ?>">
     <input type="submit" value="Enviar">
 </form>
-
 </body>
 </html>
