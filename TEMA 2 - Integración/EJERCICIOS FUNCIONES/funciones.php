f1a58c29ba69ab5e6e5d8dc5e1750652acd61dd8
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-09-26 19:18:18 +0200

    Initial commit.

---
+++ TEMA 2 - Integración/EJERCICIOS FUNCIONES/funciones.php
@@ -0,0 +1,50 @@
+<?php
+
+function concatenarPalabras(...$palabras): string
+{
+    $resultado = "";
+    foreach ($palabras as $palabra) {
+        $resultado .= $palabra . " ";
+    }
+    return trim($resultado);
+}
+
+function concatenaCon(&$cadena = " ", ...$palabras): void
+{
+    $cadena .= " " . concatenarPalabras(implode(" ", $palabras));
+}
+
+$suma = function ($a, $b) {
+    return $a + $b;
+};
+
+$resta = function ($a, $b) {
+    return $a - $b;
+};
+
+$multiplicacion = function ($a, $b) {
+    return $a * $b;
+};
+
+function aplicarOperacion($operacion, $a, $b): int
+{
+    return $operacion($a, $b);
+}
+
+aplicarOperacion($multiplicacion, 3, 4);
+
+function invertirCadena(string $cadena): string
+{
+    $resultado = "";
+    for ($i = strlen($cadena) - 1; $i >= 0; $i--) {
+        $resultado .= $cadena[$i];
+    }
+    return $resultado;
+}
+
+function invertirCadena2(string $cadena): string
+{
+    return strrev($cadena);
+}
+
+
---
+++ TEMA 2 - Integración/EJERCICIOS FUNCIONES/index.php
@@ -0,0 +1,36 @@
+<?php
+require('funciones.php');
+/**
+ * @var int $suma
+ * @var int $resta
+ * @var int $multiplicacion
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset=UTF-8">
+    <meta name="description" content="Ejercicios de funciones">
+    <meta name="author" content="Daniel Alonso Lázaro">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
+    <title>EJERCICIOS FUNCIONES</title>
+    <script src="main.js"></script>
+</head>
+<body>
+<div>
+    <?php
+    $cadena = concatenarPalabras("Hola", "que", "tal", "estas");
+    echo $cadena . "<br>";
+    concatenaCon($cadena, "yo", "muy", "bien");
+    echo $cadena . "<br>";
+    echo "3+4 es " . aplicarOperacion($suma, 3, 4) . "<br>";
+    echo "3-4 es " . aplicarOperacion($resta, 3, 4) . "<br>";
+    echo "3x4 es " . aplicarOperacion($multiplicacion, 3, 4) . "<br>";
+    echo invertirCadena($cadena) . "&nbsp;&nbsp;&nbsp;<-- con un for" . "<br>";
+    echo invertirCadena2($cadena) . "&nbsp;&nbsp;&nbsp;<-- con strrev" . "<br>";
+    ?>
+</div>
+</body>
+</html>
