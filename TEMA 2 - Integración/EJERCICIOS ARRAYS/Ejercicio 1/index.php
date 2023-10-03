4ed038d7ec40d29b0a42420099ef98cf1ddf1540
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-10-03 13:41:54 +0200

    Añadido HTML para todos los ejercicios. Añadido ejercicio 2. Cambiado estilo de lista. Añadido include del ejercicio 2.

--- TEMA 2 - Integración/EJERCICIOS ARRAYS/Ejercicio 1/index.php
+++ TEMA 2 - Integración/EJERCICIOS ARRAYS/Ejercicio 1/index.php
@@ -1,7 +1,9 @@
 <?php
 include("logicaEj1.php");
+include("logicaEj2.php");
 /**
  * @var array $frutas
+ * @var array $alumnos
  */
 ?>
 <!DOCTYPE html>
@@ -27,12 +29,35 @@ include("logicaEj1.php");
 </head>
 <body>
 <div>
-    <h4>a) Dado el array $frutas, utiliza un bucle for para imprimir cada fruta en una lista numerada.</h4>
+    <h1>EJERCICIOS ARRAYS</h1>
+    <h2>Ejercicio 1</h2>
+    <h3>i. Dado el array $frutas, utiliza un bucle for para imprimir cada fruta en una lista numerada.</h3>
     <p><?php imprimir($frutas); ?></p>
-    <h4>b) Imprime la información en una tabla.</h4>
+    <h3>ii. Imprime la información en una tabla.</h3>
     <p><?php imprimirTabla($frutas); ?></p>
-    <h4>c) Imprime la información en una tabla sin usar bucles</h4>
+    <h3>iii. Imprime la información en una tabla sin usar bucles</h3>
     <p><?php imprimirTablaSinBucles($frutas); ?></p>
+    <h2>Ejercicio 2</h2>
+    <h3>i. Dado el array $alumnos, utiliza un bucle while para encontrar al alumno más joven y mostrar su nombre y
+        edad.</h3>
+    <p><?php alumnoMasJovenWhile($alumnos); ?></p>
+    <h3>ii. Realiza la anterior tarea sin bucle.</h3>
+    <p><?php alumnoMasJoven($alumnos); ?></p>
+    <h3>iii. Muestra una tabla de los alumnos ordenados por edad.</h3>
+    <p><?php alumnosOrdenadosEdad($alumnos); ?></p>
+    <h2>Ejercicio 3</h2>
+
+    <h2>Ejercicio 4</h2>
+
+    <h2>Ejercicio 5</h2>
+
+    <h2>Ejercicio 6</h2>
+
+    <h2>Ejercicio 7</h2>
+
+    <h2>Ejercicio 8</h2>
+
+    <h2>Ejercicio 9</h2>
 </div>
 </body>
 </html>
