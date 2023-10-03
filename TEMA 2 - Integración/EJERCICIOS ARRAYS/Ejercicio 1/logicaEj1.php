07d3b6fce00391b9555b5cdfea76dbd325009a91
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-10-03 13:42:56 +0200

    Agregada variable $html. Agregada lista ordenada en ejercicio i. Agregada función substr_replace para cerrar etiqueta html.

--- TEMA 2 - Integración/EJERCICIOS ARRAYS/Ejercicio 1/logicaEj1.php
+++ TEMA 2 - Integración/EJERCICIOS ARRAYS/Ejercicio 1/logicaEj1.php
@@ -3,23 +3,27 @@ $frutas = ["Manzana", "Plátano", "Naranja", "Uva"];
 
 function imprimir($frutas): void
 {
-    foreach ($frutas as $k => $fruta) {
-        echo ($k + 1) . ".- " . $fruta . "<br>";
+    echo $html = "<ol>";
+    foreach ($frutas as $fruta) {
+        echo "<li>" . $fruta . "</li>";
     }
+    echo substr_replace($html, "/", 1, 0);
 }
 
 function imprimirTabla($frutas): void
 {
-    echo "<table>";
+    echo $html = "<table>";
     foreach ($frutas as $k => $fruta) {
         echo "<tr><td>" . ($k + 1) . "</td><td>" . $fruta . "</td></tr>";
     }
-    echo "</table>";
+    echo substr_replace($html, "/", 1, 0);
 }
 
 function imprimirTablaSinBucles($frutas): void
 {
+    echo $html = "<table>";
     array_map(function ($k, $fruta) {
         echo "<tr><td>" . ($k + 1) . "</td><td>" . $fruta . "</td></tr>";
     }, array_keys($frutas), $frutas);
+    echo substr_replace($html, "/", 1, 0);
 }
