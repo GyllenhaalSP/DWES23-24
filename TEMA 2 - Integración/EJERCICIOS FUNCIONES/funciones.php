9dca5069a43f7fe8bbe596cab7d4e2e3ca6eef3b
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-10-10 11:37:00 +0200

    Terminados ejercicios funciones.

--- TEMA 2 - Integración/EJERCICIOS FUNCIONES/funciones.php
+++ TEMA 2 - Integración/EJERCICIOS FUNCIONES/funciones.php
@@ -33,13 +33,13 @@ function aplicarOperacion($operacion, $a, $b): int
 
 aplicarOperacion($multiplicacion, 3, 4);
 
-function invertirCadena(string $cadena): string
+function invertirCadena(string &$cadena): void
 {
     $resultado = "";
     for ($i = strlen($cadena) - 1; $i >= 0; $i--) {
         $resultado .= $cadena[$i];
     }
-    return $resultado;
+    echo $cadena = $resultado;
 }
 
 function invertirCadena2(string $cadena): string
@@ -64,6 +64,58 @@ function filtra_array(callable $esPrimo, ...$numeros): array
     $resultado = [];
     foreach ($numeros as $elemento) {
         if ($esPrimo($elemento)) {
+            $resultado[] = $elemento;
+        }
+    }
+    return $resultado;
+}
+
+function magia(string $etiqueta = "li", mixed ...$elementos): void
+{
+    if ($etiqueta == "li") {
+        echo "<ul>";
+    } else {
+        echo "<$etiqueta>";
+    }
+    foreach ($elementos as $elemento) {
+        echo "<$etiqueta>$elemento</$etiqueta>";
+    }
+
+    if ($etiqueta == "li") {
+        echo "</ul>";
+    } else {
+        echo "</$etiqueta>";
+    }
+}
+
+function acumuladorGlobal(int &$acumulador, array $valores): void
+{
+    foreach ($valores as $valor) {
+        $acumulador += $valor;
+    }
+}
+
+function acumuladorGlobalVariable(int &$valor, mixed ...$valores): void
+{
+    foreach ($valores as $elemento) {
+        $valor += $elemento;
+    }
+}
+
+function acumuladorGlobalVariable2(int &$valor, mixed ...$valores): void
+{
+    $valor = array_sum($valores);
+}
+
+$esImpar = function (int $numero): bool {
+    return $numero % 2 !== 0;
+};
+
+function filtra_array2(callable $esImpar, ...$numeros): array
+{
+    $resultado = [];
+    foreach ($numeros as $elemento) {
+        if ($esImpar($elemento)) {
             $resultado[] = $elemento;
         }
     }
--- TEMA 2 - Integración/EJERCICIOS FUNCIONES/index.php
+++ TEMA 2 - Integración/EJERCICIOS FUNCIONES/index.php
@@ -5,6 +5,7 @@ require('funciones.php');
  * @var int $resta
  * @var int $multiplicacion
  * @var callable $esPrimo
+ * @var callable $esImpar
  */
 ?>
 
@@ -32,10 +33,16 @@ require('funciones.php');
     echo "3x4 es " . aplicarOperacion($multiplicacion, 3, 4) . "<br>";
     echo invertirCadena($cadena) . "&nbsp;&nbsp;&nbsp;<-- con un for" . "<br>";
     echo invertirCadena2($cadena) . "&nbsp;&nbsp;&nbsp;<-- con strrev" . "<br>";
-
     echo implode(", ", filtra_array($esPrimo, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17));
-
-    
+    echo "<br>";
+    magia('li', "Hola mundo", 3, 3.14);
+    echo "<br>";
+    $acumulador = 0;
+    acumuladorGlobal($acumulador, [1, 2, 3, 4, 5]);
+    echo $acumulador;
+    $acumulador = 0;
+    acumuladorGlobalVariable($acumulador, 1, 2, 3, 4, 5);
+    echo print_r(filtra_array2($esImpar, 5, 1, 4, 56, 7, 8, 9), true);
     ?>
 </div>
 </body>
