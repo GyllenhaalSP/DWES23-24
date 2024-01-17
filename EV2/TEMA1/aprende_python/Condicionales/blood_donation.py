a8b45cc6277cb351f13e8ce1ba5fa1b9492c974d
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2024-01-17 13:39:59 +0100

    Moved & updated

--- EV2/TEMA1/aprende_python/Condicionales/blood_donation.py
+++ EV2/TEMA1/aprende_python/Condicionales/blood_donation.py
@@ -4,9 +4,7 @@
 
 
 def run(age: int, weight: int, heartbeat: int, platelets: int) -> bool:
-    if 18 < age < 65 and weight > 50 and 60 < heartbeat < 100 and platelets > 150000:
-        return True
-    return False
+    return 18 < age < 65 and weight > 50 and 60 < heartbeat < 100 and platelets > 150000
 
 
 if __name__ == '__main__':
